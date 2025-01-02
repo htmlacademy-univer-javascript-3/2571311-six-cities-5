c9d54d7b75cc8e6b31365d204e06aa8f5ae32269
Yuliya <123246886+Yuliya3003@users.noreply.github.com>
2025-01-02 13:15:06 +0300

    add components

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+
+import { TPlaceCardEntity } from "../placeCard.typings/placeCard.typings";
+import MainPage from "../../../pages/mainPage/mainPage";
+
+type TAppProps = {
+    places: TPlaceCardEntity[];
+};
+
+function App({ places }: TAppProps): JSX.Element {
+  return <MainPage places={places} />;
+}
+
+export default App;
